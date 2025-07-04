129ef0ea746b85fc9b9859e5c9c08c8d255802dc
freitas586 <gbfreitas586@gmail.com>
2025-07-04 06:29:57 +0000

    atulizando1

--- SIMPLES/test.c
+++ SIMPLES/test.c
@@ -2,12 +2,4 @@
 #include <stdlib.h>
 
 int main(){
-int a;
-int b=5;
-int c;
-int f;
-int i;
-float d;
-instead expected token classes are []
-Error in line 11, column 18 : Syntax error
-Error in line 11, column 18 : Couldn't repair and continue parse
+Error : Variável não declarada [b]
